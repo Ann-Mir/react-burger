caac846085d5e877eb2d1b78cbccd80ee5eac20e
Anna Mironovich <anna.mail787@gmail.com>
2021-08-23 15:17:07 +0300

    adds use outside click on modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,6 +1,7 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom';
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import useOutsideClick from '../../hooks/use-outside-click';
 import ModalOverlay from '../modal-overlay/modal-overlay';
 
 import styles from './modal.module.css';
@@ -10,6 +11,14 @@ const modalRoot = document.getElementById('modal-root');
 const FOCUSABLE_ELEMENTS = 'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
 
 const Modal = ({ title, children, onClose }) => {
+
+  const modalRef = useRef(null);
+
+  const onEscKeyDown = ( { code } ) => {
+    if ( code === 'Escape' ) {
+      onClose();
+    }
+  };
 
   const onTabPressed = (firstFocusableElement, lastFocusableElement) => {
     return (evt) => {
@@ -30,17 +39,8 @@ const Modal = ({ title, children, onClose }) => {
           evt.preventDefault();
         }
       }
-
     }
-  }
-
-  useEffect(() => {
-    document.body.style.overflow = 'hidden';
-
-    return () => {
-      document.body.style.overflow = 'unset';
-    }
-  }, []);
+  };
 
   useEffect(() => {
     const modal = document.querySelector('#modal');
@@ -49,17 +49,24 @@ const Modal = ({ title, children, onClose }) => {
     const focusableContent = modal.querySelectorAll(FOCUSABLE_ELEMENTS);
     const lastFocusableElement = focusableContent[focusableContent.length - 1];
 
+    document.body.style.overflow = 'hidden';
     document.addEventListener('keydown', onTabPressed(firstFocusableElement, lastFocusableElement));
+    document.addEventListener('keydown', onEscKeyDown);
+
     firstFocusableElement.focus();
 
     return () => {
+      document.body.style.overflow = 'unset';
       document.removeEventListener('keydown', onTabPressed(firstFocusableElement, lastFocusableElement));
+      document.removeEventListener('keydown', onEscKeyDown);
     }
   }, []);
 
+  useOutsideClick(modalRef, onClose);
+
   return ReactDOM.createPortal(
     <ModalOverlay>
-      <div className={styles.modal} id="modal">
+      <div className={styles.modal} id="modal" ref={modalRef}>
         <button className={styles.close} aria-label="Закрыть модальное окно" onClick={onClose}>
           <CloseIcon type="primary"/>
         </button>
