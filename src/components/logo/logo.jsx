8894486f624e9d6f0a1c1a0dd8e5863bc0539ae3
Anna Mironovich <anna.mail787@gmail.com>
2021-10-14 14:17:28 +0300

    adds header navigation

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 import {Logo  as LogoIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+import {NavLink} from 'react-router-dom';
+import {AppRoutes} from '../../utils/constants';
 
 import styles from './logo.module.css';
 
@@ -11,9 +13,9 @@ function Logo({ className }) {
   const classes = cn(styles.logo, className);
 
   return (
-    <a href="/" className={classes}>
+    <NavLink to={AppRoutes.ROOT} className={classes} activeClassName={styles.active}>
       <LogoIcon />
-    </a>
+    </NavLink>
   )
 }
 
