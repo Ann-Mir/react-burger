c7620099b63df9e59c15395ebda7ed9dfba6c16e
Anna Mironovich <anna.mail787@gmail.com>
2021-10-20 15:03:24 +0300

    adds ingredient page

--- src/components/pages/ingredient-page/ingredient-page.jsx
+++ src/components/pages/ingredient-page/ingredient-page.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {fetchIngredients} from '../../../store/slices/ingredients-slice';
 import AppHeader from '../../app-header/app-header';
+import ErrorAlert from '../../error-alert/error-alert';
 import IngredientDetails from '../../ingredient-details/ingredient-details';
+import Spinner from '../../spinner/spinner';
 
 import styles from './ingredient-page.module.css';
 
 
 function IngredientPage() {
 
+  const dispatch = useDispatch();
+
+  const { isLoading, error } = useSelector(state => state.ingredients);
+
+  useEffect(() => {
+    dispatch(fetchIngredients());
+  }, [dispatch]);
+
   return (
     <div className={styles.wrapper}>
       <AppHeader />
-      <IngredientDetails className={styles.details}/>
+      {isLoading && <Spinner className={styles.spinner}/>}
+      {error && <ErrorAlert />}
+      {!isLoading && !error && <IngredientDetails className={styles.details}/>}
     </div>
   )
 }
