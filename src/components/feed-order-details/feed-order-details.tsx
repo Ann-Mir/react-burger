bff442b9d834a144d0bbe7eabcf8e3b454a853e5
Anna Mironovich <anna.mail787@gmail.com>
2021-11-14 20:34:37 +0300

    refactors feed order page

---
+++ src/components/feed-order-details/feed-order-details.tsx
@@ -0,0 +1,93 @@
+import cn from 'classnames';
+import React from 'react';
+import IngredientPreview from '../ingredient-preview/ingredient-preview';
+import Price from '../price/price';
+
+import styles from './feed-order-details.module.css';
+
+
+function FeedOrderDetails() {
+
+  const numberClasses = cn(styles.number, 'text text_type_digits-default');
+  const titleClasses = cn(styles.title, 'text text_type_main-medium');
+  const statusClasses = cn(styles.status, 'text text_type_main-default');
+  const ingredientsClasses = cn(styles.ingredients, 'text text_type_main-medium');
+  const nameClasses = cn(styles.name, 'text text_type_main-default');
+  const quantityClasses = cn(styles.quantity, 'text text_type_digits-default');
+
+  return (
+    <div className={styles.wrapper}>
+      <p className={numberClasses}>#054774</p>
+      <h3 className={titleClasses}>Black Hole Singularity острый бургер</h3>
+      <p className={statusClasses}>Выполнен</p>
+      <p className={ingredientsClasses}>Состав:</p>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <IngredientPreview className={styles.preview}/>
+          <p className={nameClasses}>Флюоресцентная булка R2 D2</p>
+          <p className={styles.price}>
+            <span className={quantityClasses}>2&nbsp;x&nbsp;</span>
+            <Price price={300}/>
+          </p>
+        </li>
+        <li className={styles.item}>
+          <IngredientPreview className={styles.preview}/>
+          <p className={nameClasses}>Флюоресцентная булка R2 D2</p>
+          <p className={styles.price}>
+            <span className={quantityClasses}>10&nbsp;x&nbsp;</span>
+            <Price price={3000}/>
+          </p>
+        </li>
+        <li className={styles.item}>
+          <IngredientPreview className={styles.preview}/>
+          <p className={nameClasses}>Флюоресцентная булка R2 D2</p>
+          <p className={styles.price}>
+            <span className={quantityClasses}>10&nbsp;x&nbsp;</span>
+            <Price price={3000}/>
+          </p>
+        </li>
+        <li className={styles.item}>
+          <IngredientPreview className={styles.preview}/>
+          <p className={nameClasses}>Флюоресцентная булка R2 D2</p>
+          <p className={styles.price}>
+            <span className={quantityClasses}>10&nbsp;x&nbsp;</span>
+            <Price price={3000}/>
+          </p>
+        </li>
+        <li className={styles.item}>
+          <IngredientPreview className={styles.preview}/>
+          <p className={nameClasses}>Флюоресцентная булка R2 D2</p>
+          <p className={styles.price}>
+            <span className={quantityClasses}>10&nbsp;x&nbsp;</span>
+            <Price price={3000}/>
+          </p>
+        </li>
+        <li className={styles.item}>
+          <IngredientPreview className={styles.preview}/>
+          <p className={nameClasses}>Флюоресцентная булка R2 D2</p>
+          <p className={styles.price}>
+            <span className={quantityClasses}>10&nbsp;x&nbsp;</span>
+            <Price price={3000}/>
+          </p>
+        </li>
+        <li className={styles.item}>
+          <IngredientPreview className={styles.preview}/>
+          <p className={nameClasses}>Флюоресцентная булка R2 D2</p>
+          <p className={styles.price}>
+            <span className={quantityClasses}>10&nbsp;x&nbsp;</span>
+            <Price price={3000}/>
+          </p>
+        </li>
+      </ul>
+      <div className={styles.bottom}>
+        <p className={'text text_type_main-default text_color_inactive'}>
+          Вчера, 13:50 i-GMT+3
+        </p>
+        <Price price={5000} />
+      </div>
+    </div>
+  )
+}
+
+
+export default FeedOrderDetails;
--- src/components/pages/feed-order-page/feed-order-page.tsx
+++ src/components/pages/feed-order-page/feed-order-page.tsx
@@ -1,93 +1,14 @@
 import React from 'react';
-import cn from 'classnames';
-import IngredientPreview from '../../ingredient-preview/ingredient-preview';
+import FeedOrderDetails from '../../feed-order-details/feed-order-details';
 
 import styles from './feed-order-page.module.css';
-import Price from '../../price/price';
 
 
 function FeedOrderPage() {
 
-  const numberClasses = cn(styles.number, 'text text_type_digits-default');
-  const titleClasses = cn(styles.title, 'text text_type_main-medium');
-  const statusClasses = cn(styles.status, 'text text_type_main-default');
-  const ingredientsClasses = cn(styles.ingredients, 'text text_type_main-medium');
-  const nameClasses = cn(styles.name, 'text text_type_main-default');
-  const quantityClasses = cn(styles.quantity, 'text text_type_digits-default');
-
-
   return (
     <main className={styles.main}>
-      <div className={styles.wrapper}>
-        <p className={numberClasses}>#054774</p>
-        <h3 className={titleClasses}>Black Hole Singularity острый бургер</h3>
-        <p className={statusClasses}>Выполнен</p>
-        <p className={ingredientsClasses}>Состав:</p>
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            <IngredientPreview className={styles.preview}/>
-            <p className={nameClasses}>Флюоресцентная булка R2 D2</p>
-            <p className={styles.price}>
-              <span className={quantityClasses}>2&nbsp;x&nbsp;</span>
-              <Price price={300}/>
-            </p>
-          </li>
-          <li className={styles.item}>
-            <IngredientPreview className={styles.preview}/>
-            <p className={nameClasses}>Флюоресцентная булка R2 D2</p>
-            <p className={styles.price}>
-              <span className={quantityClasses}>10&nbsp;x&nbsp;</span>
-              <Price price={3000}/>
-            </p>
-          </li>
-          <li className={styles.item}>
-            <IngredientPreview className={styles.preview}/>
-            <p className={nameClasses}>Флюоресцентная булка R2 D2</p>
-            <p className={styles.price}>
-              <span className={quantityClasses}>10&nbsp;x&nbsp;</span>
-              <Price price={3000}/>
-            </p>
-          </li>
-          <li className={styles.item}>
-            <IngredientPreview className={styles.preview}/>
-            <p className={nameClasses}>Флюоресцентная булка R2 D2</p>
-            <p className={styles.price}>
-              <span className={quantityClasses}>10&nbsp;x&nbsp;</span>
-              <Price price={3000}/>
-            </p>
-          </li>
-          <li className={styles.item}>
-            <IngredientPreview className={styles.preview}/>
-            <p className={nameClasses}>Флюоресцентная булка R2 D2</p>
-            <p className={styles.price}>
-              <span className={quantityClasses}>10&nbsp;x&nbsp;</span>
-              <Price price={3000}/>
-            </p>
-          </li>
-          <li className={styles.item}>
-            <IngredientPreview className={styles.preview}/>
-            <p className={nameClasses}>Флюоресцентная булка R2 D2</p>
-            <p className={styles.price}>
-              <span className={quantityClasses}>10&nbsp;x&nbsp;</span>
-              <Price price={3000}/>
-            </p>
-          </li>
-          <li className={styles.item}>
-            <IngredientPreview className={styles.preview}/>
-            <p className={nameClasses}>Флюоресцентная булка R2 D2</p>
-            <p className={styles.price}>
-              <span className={quantityClasses}>10&nbsp;x&nbsp;</span>
-              <Price price={3000}/>
-            </p>
-          </li>
-        </ul>
-        <div className={styles.bottom}>
-          <p className={'text text_type_main-default text_color_inactive'}>
-            Вчера, 13:50 i-GMT+3
-          </p>
-          <Price price={5000} />
-        </div>
-      </div>
+      <FeedOrderDetails />
     </main>
   )
 }
