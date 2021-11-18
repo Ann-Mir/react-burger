602147853906bb6ca99592795de309cc38c90d89
Anna Mironovich <anna.mail787@gmail.com>
2021-11-18 09:32:35 +0300

    adds data to orders feed

--- src/components/orders-feed/orders-feed.tsx
+++ src/components/orders-feed/orders-feed.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import {Link, useLocation} from 'react-router-dom';
-import {useParams} from 'react-router';
 import {useAppSelector} from '../../hooks/hooks';
-import {AppRoutes} from '../../utils/constants';
 import OrderCard from '../order-card/order-card';
 
 import styles from './orders-feed.module.css';
 
-type TParams = {
-  id: string | undefined;
-};
-
 
 function OrdersFeed(): JSX.Element {
 
-  const { id } = useParams<TParams>();
   const location = useLocation();
 
   const { orders } = useAppSelector((state) => state.feed);
@@ -23,24 +16,17 @@ function OrdersFeed(): JSX.Element {
     <section className={styles.orders}>
       <div className={styles.feed}>
         {
-          orders.map((order) => <Link
-            to={{
-              pathname: `${location.pathname}/${id}`,
-              state: {background: location}}}
-            key={order._id}
+          orders.map((order) =>
+            <Link
+              to={{
+                pathname: `${location.pathname}/${order._id}`,
+                state: {background: location}
+              }}
+              key={order._id}
             >
               <OrderCard className={styles.card} order={order} />
-          </Link>)
+            </Link>)
         }
-        {/*<Link to={{*/}
-        {/*  pathname: `${location.pathname}/${id}`,*/}
-        {/*  state: { background: location }}}>*/}
-        {/*  <OrderCard className={styles.card}/>*/}
-        {/*</Link>*/}
-        {/*<OrderCard className={styles.card}/>*/}
-        {/*<OrderCard className={styles.card}/>*/}
-        {/*<OrderCard className={styles.card}/>*/}
-        {/*<OrderCard className={styles.card}/>*/}
       </div>
     </section>
   )
